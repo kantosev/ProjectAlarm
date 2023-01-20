788e2087ed06908d013b0bb456797dfc8463c918
kantosev <103773083+kantosev@users.noreply.github.com>
2023-01-20 19:49:40 +0300

    Initial Commit

---
+++ ProjectAlarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectAlarm
+//
+//  Created by Антон Кирилюк on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
